02ef559586f860bf6038bebc41c4934a2d30677b
GeovanniRamirez2000 <104876685+GeovanniRamirez2000@users.noreply.github.com>
2022-05-11 21:34:10 -0600

    LAYOUT UNICO

--- src/java/org/models/ModelUser.java
+++ src/java/org/models/ModelUser.java
@@ -10,5 +10,114 @@ package org.models;
  * @author GEOVARU
  */
 public class ModelUser {
+
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String password;
+    private int idRol;
+    private String fechaCrea;
+    private int activo;
+    private String fechaMod;
+    private String usuarioCrea;
+    private String usuarioMod;
+    private String codigo;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getFechaCrea() {
+        return fechaCrea;
+    }
+
+    public void setFechaCrea(String fechaCrea) {
+        this.fechaCrea = fechaCrea;
+    }
+
+    public int getActivo() {
+        return activo;
+    }
+
+    public void setActivo(int activo) {
+        this.activo = activo;
+    }
+
+    public String getFechaMod() {
+        return fechaMod;
+    }
+
+    public void setFechaMod(String fechaMod) {
+        this.fechaMod = fechaMod;
+    }
+
+    public String getUsuarioCrea() {
+        return usuarioCrea;
+    }
+
+    public void setUsuarioCrea(String usuarioCrea) {
+        this.usuarioCrea = usuarioCrea;
+    }
+
+    public String getUsuarioMod() {
+        return usuarioMod;
+    }
+
+    public void setUsuarioMod(String usuarioMod) {
+        this.usuarioMod = usuarioMod;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
     
 }
